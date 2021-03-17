9a2f18419b4d1eb291890aff66049aaf16749b3a
Vito Gentile <gentilevito1076@gmail.com>
2021-03-17 16:32:30 +0100

    Correzione su stringa Use

--- app/Http/Controllers/TheController.php
+++ app/Http/Controllers/TheController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App/Car;
+use App\Car;
 
 class TheController extends Controller
 {
     public function index() {
+      $cars = Car::all();
+      dump($cars);
       return view('list');
     }
 }
